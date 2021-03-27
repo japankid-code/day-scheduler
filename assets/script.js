1e9eba4f96067896187b8c25101107792c59b768
japankid-code <japankid.jake@gmail.com>
2021-03-27 18:56:41 -0500

    rearranging things, add script.js

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// the current day is displayed at the top of the calendar
+// scrolling down presents time blocks for standard business hours
+// each block is color-coded to indicate whether it is in the past, present, or future
+// click into a time block, can enter an event
+// clicking save button for time block, the text from that event is saved in localStorage
+// refreshing the page the saved events persist
