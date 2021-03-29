375bf4443ae0249b109789f05a40e4112d6e7e45
japankid-code <japankid.jake@gmail.com>
2021-03-28 22:04:55 -0500

    current date displays on top

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,32 @@
+// gather the selectors needed from the html
+let currentDayEl = document.getElementById("current-day");
+let timeBlockBoxEl = document.getElementById("time-block-box");
+// let buddhistEra = ('https://cdn.jsdelivr.net/npm/dayjs/plugin/buddhistEra.js');
+// dayjs.extend(buddhistEra);
+// dayjs().format('BBBB BB');
+
 // the current day is displayed at the top of the calendar
+const showDate = function() {
+    let d = new Date();
+    let bd = new Date(2021, 9, 01);
+    let now = dayjs(d);
+    let nowEl = document.createElement("span")
+    nowEl.textContent = `${now}`;
+    console.log(now);
+    currentDayEl.appendChild(nowEl);
+}
+
 // scrolling down presents time blocks for standard business hours
+
 // each block is color-coded to indicate whether it is in the past, present, or future
+
 // click into a time block, can enter an event
+
 // clicking save button for time block, the text from that event is saved in localStorage
-// refreshing the page the saved events persist
+
+// refreshing the page, the saved events persist
+const schedLoader = function() {
+    
+}
+
+showDate();
