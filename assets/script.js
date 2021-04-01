e60de388b54d49c82a2f1850709b58a59bf869fe
japankid-code <japankid.jake@gmail.com>
2021-04-01 17:20:17 -0500

    clicking the note turns it into an editable textbox

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,26 @@
 // gather the selectors needed from the html
 const currentDayEl = document.getElementById("current-day");
 const timeBlockBoxEl = document.getElementById("time-block-box");
+let notesObj = {
+    09: ``,
+    10: ``,
+    11: ``,
+    12: ``,
+    13: ``,
+    14: ``,
+    15: ``,
+    16: ``,
+    17: ``
+};
 
 const customUtcOffset = 240; //mins
 
-dayjs.extend(window.dayjs_plugin_buddhistEra)
-dayjs.extend(window.dayjs_plugin_utc)
-dayjs().format();
 
-let day = dayjs()
-
-console.log(`${day}`)
 
 // the current day is displayed at the top of the calendar
 const showDate = () => {
-    let d = dayjs();
-    let bd = dayjs('2021', '09', '01');
-    let now = dayjs(d).utcOffset(customUtcOffset);
+
+    let now = dayjs().format();
     let nowEl = document.createElement("span")
     nowEl.textContent = `${now}`;
     currentDayEl.appendChild(nowEl);
@@ -27,11 +31,11 @@ const renderHours = () => {
     // loop thru hours in a day
     for (let i = 9; i < 18; i++) {
         let hour = i;
-        let meridiem = `ᵃᵐ`;
+        let meridiem = `ᵃᵐ`; // for the AMs
         if (i > 11) {
-            meridiem = `ᵖᵐ`;
+            meridiem = `ᵖᵐ`; // for the PMs
             if (i > 12) {
-                hour -= 12;
+                hour -= 12; // for the afternoon :)
             }
         }
         // create the row element here
@@ -43,36 +47,58 @@ const renderHours = () => {
         hourDisplay.textContent = `${hour}${meridiem}`;
         hourDisplay.setAttribute("data-hour", `${i}`);
         // notes column
-        let hmmsDisplay = document.createElement(`div`);
-        hmmsDisplay.classList = `note-card card col-10 border-left`;
-        hmmsDisplay.setAttribute("data-hour", `${i}`);
+        let noteBoxEl = document.createElement(`div`);
+        noteBoxEl.classList = `note-card card col-10 border-left`;
+        noteBoxEl.setAttribute("data-hour", `${i}`);
         let notesEl = document.createElement('p')
         notesEl.setAttribute("data-hour", `${i}`);
-        hmmsDisplay.append(notesEl);
+        notesEl.textContent = `something at ${i}`;
+        notesEl.classList = `note card-body`;
+        noteBoxEl.append(notesEl);
 
         // save button column
         let buttonCol = document.createElement("div")
         buttonCol.classList = `col-1 border-left border-right d-flex flex-column justify-content-center`;
         let saveButton = document.createElement("p");
-        saveButton.innerHTML = `<i class="fas fa-burn"></i>`;
+        saveButton.innerHTML = `<i class="save-button fas fa-burn"></i>`;
         saveButton.setAttribute("data-hour", `${i}`);
         buttonCol.append(saveButton);
 
-        hourRowEl.append(hourDisplay, hmmsDisplay, buttonCol);
+        hourRowEl.append(hourDisplay, noteBoxEl, buttonCol);
         timeBlockBoxEl.appendChild(hourRowEl);
     }   
 
-    // each timeblock has an input field and save button
-
-    // clicking time block's save button saves the input text to local storage
 }
-    
 
 // each block is color-coded to indicate whether it is in the past, present, or future
 
-// click into a time block, can enter an event
+// this handler changes the p ele to a textarea and enable saving the contents
+// and reconverting to p ele after clicking the button
+let saveHandler = (e) => {
+    // click into a time block, can enter an event    
+    if (e.target.classList.contains('note')) {
+        let noteEl = e.target;
+        let hour = e.target.getAttribute(`data-hour`);
+        let note = e.target.textContent;
+        console.log(noteEl);
+        noteEl.textContent = '';
+        // when clicking the note, will replace it with a textarea element that has the same 
+        e.target.textContent = ``;
+        let noteEditEl = document.createElement("textarea");
+        noteEditEl.textContent = `${note}`
+        noteEditEl.setAttribute("id", "note-edit");
+        e.target.appendChild(noteEditEl)
+        
+    }
+    // clicking save button for time block, the text from that event is saved in localStorage
+    // and the textarea gets converted back into a p ele
+    if (e.target.classList.contains(`save-button`)) {
+        let hour = e.target.getAttribute(`data-hour`);
+        
+    }
+}
 
-// clicking save button for time block, the text from that event is saved in localStorage
+
 
 // refreshing the page, the saved events persist
 const schedLoader = function() {
@@ -83,12 +109,5 @@ showDate();
 renderHours();
 
 
-
-$(".list-group").on("click", "p", function() {
-    var text = $(this).text();
-    // clicking on the p will change it to a textarea
-    var textInput = $("<textarea>").addClass("form-control").val(text);
-    $(this).replaceWith(textInput);
-    textInput.trigger("focus");
-    // click off the textarea
-  });
+// adds an event listener to the div holding the notes element
+timeBlockBoxEl.addEventListener("click", (e) => {saveHandler(e)});
